351083f2df4f76094c9dc24a5b16f143023dfc46
kc <kyungcheolkoh@gmail.com>
2020-10-27 11:57:35 +0900

    adding signup and login feature
    testing is required

---
+++ app/src/main/java/com/Alzreminder_caregiver/MainActivity.java
@@ -0,0 +1,17 @@
+package com.Alzreminder_caregiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.parse.ParseObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
