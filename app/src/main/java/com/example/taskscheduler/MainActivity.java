c8455092af18062dcf3c340960e0f4edcc2f899a
unknown <jacobspettigrew@gmail.com>
2020-10-30 21:48:15 -0700

    Test backup

--- app/src/main/java/com/example/taskscheduler/MainActivity.java
+++ app/src/main/java/com/example/taskscheduler/MainActivity.java
@@ -2,13 +2,74 @@ package com.example.taskscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the UI elements by their id
+        listView = findViewById(R.id.listView);
+        addButton = findViewById(R.id.button);
+
+        // Adds a task to the task list when the add button is clicked
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        // The list that will hold the tasks
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+    }
+
+    // Watches for a long press that will remove an task from the task list
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Task Removed", Toast.LENGTH_LONG).show();
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    // When called, it will add the task to the list
+    private void addItem(View view) {
+        EditText input = findViewById(R.id.editTextTextPersonName);
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(""))) {
+            itemsAdapter.add(itemText);
+            input.setText("");
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Please enter task", Toast.LENGTH_LONG).show();
+        }
     }
 }
