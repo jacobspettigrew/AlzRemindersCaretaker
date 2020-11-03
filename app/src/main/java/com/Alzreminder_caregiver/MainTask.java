ca73adfb6d935beb703664207b93844bf7dbcc5b
kc <kyungcheolkoh@gmail.com>
2020-11-04 00:34:51 +0900

    refactoring code
    new function for findObjectid

--- app/src/main/java/com/Alzreminder_caregiver/MainTask.java
+++ app/src/main/java/com/Alzreminder_caregiver/MainTask.java
@@ -14,7 +14,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
@@ -23,21 +28,27 @@ import java.util.List;
 
 public class MainTask extends AppCompatActivity {
 
+    //var
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
+    ParseObject patientObject;
+
+    //ui
     private ListView listView;
     private Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tasks_jacob);
 
-        // Find the UI elements by their id
         listView = findViewById(R.id.listView);
         addButton = findViewById(R.id.button);
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        findObjectId();
 
         // Adds a task to the task list when the add button is clicked
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -47,24 +58,9 @@ public class MainTask extends AppCompatActivity {
             }
         });
 
-        items = new ArrayList<>();
-        List<Object> item =  ParseUser.getCurrentUser().getList("arrayToDos");
-        if(item == null){
-
-        }
-        else{
-            for (int i = 0; i < item.size(); i++) {
-                items.add(item.get(i).toString());
-            }
-        }
-
-
-
-        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(itemsAdapter);
         setUpListViewListener();
     }
-
 
     // Watches for a long press that will remove an task from the task list
     private void setUpListViewListener() {
@@ -73,16 +69,16 @@ public class MainTask extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 Context context = getApplicationContext();
                 Toast.makeText(context, "Task Removed", Toast.LENGTH_LONG).show();
+
                 items.remove(position);
                 itemsAdapter.notifyDataSetChanged();
-
-                ParseUser.getCurrentUser().put("arrayToDos", items);
-                ParseUser.getCurrentUser().saveInBackground();
+                //upadate in the data
+                patientObject.put("arrayToDos", items);
+                patientObject.saveInBackground();
                 return true;
             }
         });
     }
-
 
     // When called, it will add the task to the list
     private void addItem(View view) {
@@ -92,12 +88,40 @@ public class MainTask extends AppCompatActivity {
         if(!(itemText.equals(""))) {
             itemsAdapter.add(itemText);
 
-            ParseUser.getCurrentUser().addAllUnique("arrayToDos", items);
-            ParseUser.getCurrentUser().saveInBackground();
+            //update in the data
+            patientObject.addAllUnique("arrayToDos",items);
+            patientObject.saveInBackground();
             input.setText("");
         }
         else {
             Toast.makeText(getApplicationContext(), "Please enter task", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void findObjectId(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Patient");
+
+        query.whereEqualTo("objectId", ParseUser.getCurrentUser().getString("patientId"));
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    if (objects.size() > 0) {
+                        for (ParseObject object : objects) {
+                            patientObject = object;
+                            List<Object> item = object.getList("arrayToDos");
+                            if (item == null) {
+                            }
+                            else {
+                                for (int i = 0; i < item.size(); i++) {
+                                    items.add(item.get(i).toString());
+                                    itemsAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 }
