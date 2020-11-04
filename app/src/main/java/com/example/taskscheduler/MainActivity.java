aaa22778f5a25781be344c938ac5f3f56798b87a
unknown <jacobspettigrew@gmail.com>
2020-11-03 21:31:44 -0800

    Rename main activity

--- app/src/main/java/com/example/taskscheduler/MainActivity.java
+++ app/src/main/java/com/example/taskscheduler/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_caregiver_task_scheduler);
 
         // Find the UI elements by their id
         listView = findViewById(R.id.listView);
